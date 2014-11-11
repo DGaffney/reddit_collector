53492e843ff7d4a96afd774c9b9037637087ef13
Devin Gaffney <itsme@devingaffney.com>
2014-11-11 13:36:44 -0500

    updates

--- workers/crawler.rb
+++ workers/crawler.rb
@@ -8,16 +8,18 @@ class Crawler
     ".json"
   end
 
-  def perform
-    data = JSON.parse(RestClient.get(url+crawl_format+"?count=100"))
+  def perform(thread_group)
+    count = 100
+    data = JSON.parse(RestClient.get(url+thread_group+crawl_format))
     while !data.data.after.nil?
+      count += 100
       data.data.children.collect(&:data).each do |thread|
         t = RedditThread.first_or_create(reddit_id: thread.id)
         t.content = thread
         t.save!
         CollectThread.perform_async(thread.id)
       end
-      data = JSON.parse(RestClient.get(url+crawl_format+"?count=100&after="+data.data.after))
+      data = JSON.parse(RestClient.get(url+crawl_format+"?count=#{count}&after="+data.data.after))
     end
   end
 end
